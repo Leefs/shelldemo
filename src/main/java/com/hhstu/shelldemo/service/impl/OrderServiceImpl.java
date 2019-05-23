3fe00eb3ce1ee32463b42851c66ede2a10e6e3d8
Leefs <2556704509@qq.com>
2019-05-23 17:40:15 +0800

    买家端Service层开发

--- src/main/java/com/hhstu/shelldemo/service/impl/OrderServiceImpl.java
+++ src/main/java/com/hhstu/shelldemo/service/impl/OrderServiceImpl.java
@@ -156,7 +156,7 @@ public class OrderServiceImpl implements OrderService {
 
         //如果已支付，需要退款
         if(orderDTO.getPayStatus().equals(PayStatusEnum.SUCCESS.getCode())){
-            //TODO
+            //TODOO
         }
         return orderDTO;
     }
